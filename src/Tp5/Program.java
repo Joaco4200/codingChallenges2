41e240c8075e3e4ae654991f132f3ee25943f1b5
Joaquin Scarone <joaquinscarone1012@gmail.com>
2025-04-11 19:01:39 -0300

    Divide and conquer new recursive function

--- src/Tp5/Program.java
+++ src/Tp5/Program.java
@@ -5,5 +5,6 @@ public class Program {
 	public static void main(String[] ags) {
 		
 		System.out.println(RecursiveFunctionsDaC.reverseString("Hello"));
+		System.out.println(RecursiveFunctionsDaC.sumbinaryDigits("10101011"));
 	}
 }
