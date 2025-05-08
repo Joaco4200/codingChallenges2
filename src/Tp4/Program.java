cccc2c5065594c6b04b6e481ff91f87bd08f59b3
Joaquin Scarone <joaquinscarone1012@gmail.com>
2025-05-08 19:53:22 -0300

    clean programs

--- src/Tp4/Program.java
+++ src/Tp4/Program.java
@@ -5,17 +5,5 @@ import java.util.ArrayList;
 public class Program {
 
 	public static void main(String[] args) {
-		
-		int num=2;
-		System.out.println(RecursiveFunctions.itsevenorOdd(num));
-		System.out.println(RecursiveFunctions.evenDigits(num));
-		System.out.println(RecursiveFunctions.sumDigits(num));
-		System.out.println(RecursiveFunctions.stringReverse("Hellooo"));
-		System.out.println(RecursiveFunctions.factorial(5));
-		System.out.println(RecursiveFunctions.sumNaturals(5));
-		System.out.println(RecursiveFunctions.numString(5));
-		System.out.println(RecursiveFunctions.sumbinaryDigits("101011"));
-		System.out.println(RecursiveFunctions.fibonacci(7));
-		RecursiveFunctions.permutations("abc", "");
 	}	
 }
--- src/Tp5/Program.java
+++ src/Tp5/Program.java
@@ -3,8 +3,5 @@ package Tp5;
 public class Program {
 	
 	public static void main(String[] ags) {
-		
-		System.out.println(RecursiveFunctionsDaC.reverseString("Hello"));
-		System.out.println(RecursiveFunctionsDaC.sumbinaryDigits("10101011"));
 	}
 }
