818f1e752c7b1d42f2d1fd2cd32caeec548bce75
Joaquin Scarone <joaquinscarone1012@gmail.com>
2025-06-10 09:43:53 -0300

    hashing

--- src/Hashing/Program.java
+++ src/Hashing/Program.java
@@ -1,17 +1,5 @@
 package Hashing;
 
-
-import java.util.LinkedList;
-import java.util.Map;
-
-
 public class Program {
-	
-	
-	static LinkedList<Product>[] tablaHash = new LinkedList[13];
-	 
-	for (int i = 0; i < 13; i++) {
-	    tablaHash[i] = new LinkedList<>();
-	}
-}
+
 }
